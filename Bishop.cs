34c12b51931f89a3893afe9b896d08243efb49b9
dinath2 <65164585+dinath2@users.noreply.github.com>
2020-11-11 11:22:02 +0100

    yup

---
+++ Bishop.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleChess
+{
+    internal class Bishop : Piece
+    {
+        public Bishop(string symbol) : base(symbol)
+        {
+            Symbol = symbol;
+        }
+
+        public override bool Move(string fromPosition, string toPosition)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Rook.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleChess
+{
+    internal class Rook : Piece
+    {
+        public Rook(string symbol) : base(symbol)
+        {
+
+        }
+
+        public override bool Move(string fromPosition, string toPosition)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
